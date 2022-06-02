8825b70c71bc31fba7ca24b4954b4a023ae86179
jean paul <jeanpaulabizeid@gmail.com>
2022-06-02 09:44:04 +0300

    creating popup to take changes

--- scripts/script.js
+++ scripts/script.js
@@ -124,9 +124,41 @@ $(document).ready(function(){
             createRow(id, title, desc, point, time);
 
             //resetting values
-            reset();}else{
+            reset();
+            
+            btn.length += 1;
+            // console.log(btn.length);
+        }else{
                 alert("Missing information, please fill all the form")
             }
     })
 })
 
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementsByClassName("change");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close");
+
+//checkbox for showing password
+var show = document.getElementById("toggle");
+
+// When the user clicks the button, open the modal
+for (var j = 0; j < btn.length; j++) {
+    btn[j].addEventListener("click", function(){
+        modal.style.display = "block";})
+} 
+
+// When the user clicks on <span> (x), close the modal
+span[0].addEventListener("click", function(){
+    modal.style.display = "none";})
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+if (event.target == modal) {
+    modal.style.display = "none";
+}
+}
